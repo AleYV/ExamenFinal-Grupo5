fa25b8e0919f10304fd5558493dd36658e9799af
Italo Silva Guanilo <italosilva@uni.pe>
2022-08-05 13:30:19 -0500

    Prueba automatizada patron decorator

--- src/test/java/Class/Decorator/ProductDecoratorTest.java
+++ src/test/java/Class/Decorator/ProductDecoratorTest.java
@@ -3,31 +3,77 @@ package Class.Decorator;
 import Class.Product.AbstractProduct;
 import Class.Product.Hamburger;
 import Class.Product.Pizza;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductDecoratorTest {
-    AbstractProduct pizza = new Pizza("base",10.2f,null);
-    AbstractProduct hamburger = new Hamburger("normal", 5.5f, null);
+    @Nested
+    class DecoratorTestPizza{
+        float price = 10.2f;
+        float result;
 
-    @Test
-    public void testCalculateCostPizza(){
-        NeapolitanPizza neapolitanPizza = new NeapolitanPizza(pizza);
-        CarbonaraPizza carbonaraPizza = new CarbonaraPizza(pizza);
-        float result = neapolitanPizza.calculateCost();
-        assertEquals(pizza.getPrice()*1.4f,result);
-        result = carbonaraPizza.calculateCost();
-        assertEquals(pizza.getPrice()*1.3f,result);
+        @DisplayName("Pizza creada con atributos especificos")
+        @Test
+        public void testCalculateCost() {
+            AbstractProduct pizza = new Pizza("base", price, null);
+            NeapolitanPizza neapolitanPizza = new NeapolitanPizza(pizza);
+            CarbonaraPizza carbonaraPizza = new CarbonaraPizza(pizza);
+            result = neapolitanPizza.calculateCost();
+            assertEquals(pizza.getPrice() * 1.4f, result);
+            result = carbonaraPizza.calculateCost();
+            assertEquals(pizza.getPrice() * 1.3f, result);
+        }
+
+        @DisplayName("Pizza creada con atributos random")
+        @Test
+        public void testCalculateVariable() {
+            Random rng = new Random();
+            price = rng.nextFloat(0, 10);
+            AbstractProduct pizza = new Pizza("base", price, null);
+            NeapolitanPizza neapolitanPizza = new NeapolitanPizza(pizza);
+            result = neapolitanPizza.calculateCost();
+            assertEquals(pizza.getPrice() * 1.4f, result);
+            CarbonaraPizza carbonaraPizza = new CarbonaraPizza(pizza);
+            result = carbonaraPizza.calculateCost();
+            assertEquals(pizza.getPrice() * 1.3f, result);
+        }
+
     }
 
-    @Test
-    public void testCalculateCostHamburger(){
-        VegetarianHamburger vegetarianHamburger = new VegetarianHamburger(hamburger);
-        RanchHamburger ranchHamburger = new RanchHamburger(hamburger);
-        float result = vegetarianHamburger.calculateCost();
-        assertEquals(hamburger.getPrice()*1.5f,result);
-        result = ranchHamburger.calculateCost();
-        assertEquals(hamburger.getPrice()*1.2f,result);
+    @Nested
+    class DecoratorTestHamburger {
+        float price = 5.5f;
+        float result;
+
+        @DisplayName("Hamburguesa creada con atributos especificos")
+        @Test
+        public void testCalculateCost() {
+            AbstractProduct hamburger = new Hamburger("normal", price, null);
+            VegetarianHamburger vegetarianHamburger = new VegetarianHamburger(hamburger);
+            RanchHamburger ranchHamburger = new RanchHamburger(hamburger);
+            result = vegetarianHamburger.calculateCost();
+            assertEquals(hamburger.getPrice() * 1.5f, result);
+            result = ranchHamburger.calculateCost();
+            assertEquals(hamburger.getPrice() * 1.2f, result);
+        }
+
+        @DisplayName("Hamburguesa creada con atributos random")
+        @Test
+        void testCalculatorVariable() {
+            Random rng = new Random();
+            price = rng.nextFloat(0, 10);
+            AbstractProduct hamburger = new Hamburger("normal", price, null);
+            VegetarianHamburger vegetarianHamburger = new VegetarianHamburger(hamburger);
+            RanchHamburger ranchHamburger = new RanchHamburger(hamburger);
+            result = vegetarianHamburger.calculateCost();
+            assertEquals(hamburger.getPrice() * 1.5f, result);
+            result = ranchHamburger.calculateCost();
+            assertEquals(hamburger.getPrice() * 1.2f, result);
+        }
     }
 }
